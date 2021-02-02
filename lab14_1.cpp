c3e597c6293f1ec2440022ccbbe6a4043f6c9246
thegenggo <kritsanaphong-keang@hotmail.com>
2021-02-02 14:54:37 +0700

    lab14_1.cpp Update

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,22 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
-	
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
+	cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z <<"\n" ;
+	cout << &a << " " << (void *)&b << " " << (void *)&c << " " << &x << " " << &y << " " << &z <<"\n" ;
+	b = 'F';
+	cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z <<"\n" ;
+	*y = 'W';
+	cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z <<"\n" ;
+	*x = 6;
+	cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z <<"\n" ;
+	**z = 7;
+	cout << a << " " << b << " " << c << " " << x << " " << (void *)y << " " << z <<"\n" ;
 	return 0;
 }
